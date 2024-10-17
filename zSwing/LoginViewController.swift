f63e71153a46a719f360988990190a833f68b32f
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2024-10-17 17:23:26 +0900

    구글로그인

---
+++ zSwing/LoginViewController.swift
@@ -0,0 +1,197 @@
+//
+//  LoginViewController.swift
+//  zSwing
+//
+//  Created by USER on 10/17/24.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+import KakaoSDKAuth
+import KakaoSDKUser
+import AuthenticationServices
+
+class LoginViewController: UIViewController {
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "logo") // 로고 이미지를 추가하세요
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let kakaoLoginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("카카오 로그인", for: .normal)
+        button.backgroundColor = .yellow
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let googleLoginButton: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.style = .standard
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let appleLoginButton: ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupActions()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(logoImageView)
+        view.addSubview(kakaoLoginButton)
+        view.addSubview(googleLoginButton)
+        view.addSubview(appleLoginButton)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            kakaoLoginButton.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 50),
+            kakaoLoginButton.widthAnchor.constraint(equalToConstant: 250),
+            kakaoLoginButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            googleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            googleLoginButton.topAnchor.constraint(equalTo: kakaoLoginButton.bottomAnchor, constant: 20),
+            googleLoginButton.widthAnchor.constraint(equalToConstant: 250),
+            googleLoginButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            appleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appleLoginButton.topAnchor.constraint(equalTo: googleLoginButton.bottomAnchor, constant: 20),
+            appleLoginButton.widthAnchor.constraint(equalToConstant: 250),
+            appleLoginButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    private func setupActions() {
+        kakaoLoginButton.addTarget(self, action: #selector(kakaoLoginTapped), for: .touchUpInside)
+        googleLoginButton.addTarget(self, action: #selector(googleLoginTapped), for: .touchUpInside)
+//        appleLoginButton.addTarget(self, action: #selector(appleLoginTapped), for: .touchUpInside)
+    }
+    
+    @objc private func kakaoLoginTapped() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오톡 로그인 성공")
+                    // 추가 처리
+                }
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오 계정 로그인 성공")
+                    // 추가 처리
+                }
+            }
+        }
+    }
+    
+    @objc private func googleLoginTapped() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print("Google Sign-In error: \(error!.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                print("Failed to get user or ID token")
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Firebase sign-in error: \(error.localizedDescription)")
+                } else {
+                    print("Google 로그인 성공")
+                    // 추가 처리
+                }
+            }
+        }
+    }
+//    @objc private func appleLoginTapped() {
+//        let appleIDProvider = ASAuthorizationAppleIDProvider()
+//        let request = appleIDProvider.createRequest()
+//        request.requestedScopes = [.fullName, .email]
+//        
+//        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+//        authorizationController.delegate = self
+//        authorizationController.presentationContextProvider = self
+//        authorizationController.performRequests()
+//    }
+}
+
+//extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+//    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+//        return self.view.window!
+//    }
+//    
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+//        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+//            guard let nonce = currentNonce else {
+//                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+//            }
+//            guard let appleIDToken = appleIDCredential.identityToken else {
+//                print("Unable to fetch identity token")
+//                return
+//            }
+//            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+//                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+//                return
+//            }
+//            
+//            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+//                                                      idToken: idTokenString,
+//                                                      rawNonce: nonce)
+//            
+//            Auth.auth().signIn(with: credential) { authResult, error in
+//                if let error = error {
+//                    print("Firebase sign-in error: \(error.localizedDescription)")
+//                    return
+//                }
+//                print("Apple 로그인 성공")
+//                // 추가 처리
+//            }
+//        }
+//    }
+//    
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+//        print("Apple Sign-In error: \(error.localizedDescription)")
+//    }
+//}
