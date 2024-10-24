f6ead9c7db02b18f7c92e1b2e3753783afa1b693
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2024-10-24 11:07:01 +0900

    현재위치 표시

--- zSwing/MainTabBarController.swift
+++ zSwing/MainTabBarController.swift
@@ -12,6 +12,12 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewControllers()
+        configureTabBar()
+        
+        // 모든 네비게이션 컨트롤러의 네비게이션 바 숨기기
+        viewControllers?.forEach { navigationController in
+            (navigationController as? UINavigationController)?.navigationBar.isHidden = true
+        }
     }
 
     private func setupViewControllers() {
@@ -25,5 +31,25 @@ class MainTabBarController: UITabBarController {
         profileVC.tabBarItem = UITabBarItem(title: "내 정보", image: UIImage(systemName: "person"), tag: 2)
 
         viewControllers = [mapVC, photoVC, profileVC].map { UINavigationController(rootViewController: $0) }
+    }
+    
+    private func configureTabBar() {
+        // iOS 15 이상에서 탭바 배경을 불투명하게 설정
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()  // 불투명 배경 설정
+            
+            // 탭바 그림자 효과 제거 (선택사항)
+            appearance.shadowColor = nil
+            
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = appearance  // 스크롤 시에도 동일한 모습 유지
+        } else {
+            // iOS 15 미만에서는 아래 방식으로 설정
+            tabBar.isTranslucent = false
+        }
+        
+        // 탭바 배경색 설정 (원하는 색상으로 변경 가능)
+        tabBar.backgroundColor = .systemBackground
     }
 }
--- zSwing/MapViewController.swift
+++ zSwing/MapViewController.swift
@@ -6,11 +6,154 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    private let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        return manager
+    }()
+    
+    private let currentLocationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        button.backgroundColor = .white
+        button.tintColor = .systemBlue
+        button.layer.cornerRadius = 20
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 0, height: 2)
+        button.layer.shadowRadius = 2
+        button.layer.shadowOpacity = 0.3
+        return button
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupLocationManager()
+        addSamplePlaygrounds()
+    }
+    
+    // MARK: - Setup
+    private func setupUI() {
         view.backgroundColor = .white
         title = "지도"
+        
+        // Add MapView
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // Add Current Location Button
+        view.addSubview(currentLocationButton)
+        NSLayoutConstraint.activate([
+            currentLocationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            currentLocationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            currentLocationButton.widthAnchor.constraint(equalToConstant: 40),
+            currentLocationButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        // Add target to button
+        currentLocationButton.addTarget(self, action: #selector(currentLocationButtonTapped), for: .touchUpInside)
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: - Actions
+    @objc private func currentLocationButtonTapped() {
+        locationManager.startUpdatingLocation()
+        
+        // 버튼 탭했을 때 시각적 피드백
+        UIView.animate(withDuration: 0.1, animations: {
+            self.currentLocationButton.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }) { _ in
+            UIView.animate(withDuration: 0.1) {
+                self.currentLocationButton.transform = .identity
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    private func addSamplePlaygrounds() {
+        let playgrounds = [
+            ["name": "어린이대공원 놀이터", "latitude": 37.5509, "longitude": 127.0817],
+            ["name": "상암문화광장 놀이터", "latitude": 37.5665, "longitude": 126.8984],
+        ]
+        
+        for playground in playgrounds {
+            let annotation = MKPointAnnotation()
+            annotation.title = playground["name"] as? String
+            annotation.coordinate = CLLocationCoordinate2D(
+                latitude: playground["latitude"] as! CLLocationDegrees,
+                longitude: playground["longitude"] as! CLLocationDegrees
+            )
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    private func zoomToUserLocation(location: CLLocation) {
+        let region = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000
+        )
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        zoomToUserLocation(location: location)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("위치 업데이트 실패: \(error.localizedDescription)")
+    }
+}
+
+// MARK: - MKMapViewDelegate
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let identifier = "PlaygroundPin"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
     }
 }
